8f0f9dc978d2069c8bb187980719cd0941347665
saber2pr <saber2pr@gmail.com>
2018-11-26 11:25:58 +0800

    update

--- assets/script/api_i.ts
+++ assets/script/api_i.ts
@@ -3,9 +3,11 @@ export interface IplayerData {
 }
 
 export function getPlayerData(): IplayerData {
+  // 此处报错请忽略
   return playerData
 }
 
 export function postPlayerData({ money }: IplayerData): void {
+  // 此处报错请忽略
   playerData.money = money
 }
